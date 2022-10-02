9f934b8ca9d076337b458448ba641482f730c9dd
Jaxon <mohamedmostels@gmail.com>
2022-10-02 12:05:42 +0200

    Update main.tf

--- terraform-in-action/ch04-mult-tiered-web-app/modules/networking/main.tf
+++ terraform-in-action/ch04-mult-tiered-web-app/modules/networking/main.tf
@@ -6,9 +6,9 @@ module "vpc" {
   name                         = "${var.namespace}-vpc"
   cidr                         = "10.0.0.0/16"
   azs                          = data.aws_availability_zones.available.names
-  private_subnets              = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets               = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  database_subnets             = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+  public_subnets               = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets              = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  database_subnets             = ["10.0.111.0/24", "10.0.112.0/24", "10.0.113.0/24"]
   create_database_subnet_group = true
   enable_nat_gateway           = true
   single_nat_gateway           = true
@@ -19,7 +19,7 @@ module "vpc" {
 }
 
 resource "aws_security_group" "load_balancer_sg" {
-  name        = "load_balancer_sg"
+  name        = "load-balancer-sg"
   description = "Allow HTTP inbound traffic"
   vpc_id      = module.vpc.vpc_id
 
